e93ca89b65a2dd19cf9ea4919565e1fe390acc87
Rimma27 <rimmaady@yandex.ru>
2023-01-12 17:28:57 +0300

    crcard

--- controllers/cards.js
+++ controllers/cards.js
@@ -9,7 +9,7 @@ module.exports.getCards = (req, res) => {
 
 module.exports.createCard = (req, res) => {
   const { name, link } = req.body;
-  const { owner } = req.user._id;
+  const owner = req.user._id;
   Card.create({ name, link, owner })
     .then((card) => res.status(201).send({ card }))
     .catch((err) => {
